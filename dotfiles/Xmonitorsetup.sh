646c22456a9f81f3ae7ef358d908fdf51126c191
Sophie Au <sophieau@users.noreply.github.com>
2017-11-23 21:34:14 +0100

    Fixes xrandr naming error that came with f27

--- dotfiles/Xmonitorsetup.sh
+++ dotfiles/Xmonitorsetup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-intern=eDP1
-extern=HDMI1
+intern=eDP-1
+extern=HDMI-1
 
 # if external monitor is connected, turn on multi-screen display, else turn on single-screen
 if xrandr | grep "$extern disconnected"; then
-	xrandr --output HDMI1 --off --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+	xrandr --output HDMI-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 	echo "single"
 else
-	xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+	xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
 	echo "multi"
 fi
 
